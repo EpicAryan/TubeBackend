a7ae0108c64cabcae562ec543e5acd532f5be990
EpicAryan <adaryan0811@gmail.com>
2024-05-02 07:19:56 +0530

    feat: connect to database

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
-console.log('Hello, world!')
+// require('dotenv').config({path: './env'})
+
+import dotenv from "dotenv"
+
+import connectDB from "./db/index.js";
+
+
+dotenv.config({ path: "./env" });
+
+connectDB();
+
+// basic approach to connect a db
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants.js";
+// import express from "express";
+// const app = express();
+// ;(async () => {
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+//         app.on("error", (err) => {
+//             console.log("Error: ",err);
+//             throw err;
+//         });
+//         app.listen(process.env.PORT, () => {
+//             console.log(`Server is running on port ${process.env.PORT}`);
+//         });
+//     } catch (error) {
+//         console.log("Error: ",error);
+//         throw error;
+//     }
+// })()
